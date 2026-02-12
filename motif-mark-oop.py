a06fcad23908bc3731524f765e30e6eeef406cc2
vini-karumuru <vkarumuru07@gmail.com>
2026-02-11 17:43:45 -0800

    setting up drawing surface & drawing introns

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -2,7 +2,9 @@
 
 # import libraries
 import argparse
+import re
 import cairo
+
 
 # set global variables to hold inputs
 def get_args():
@@ -11,7 +13,6 @@ def get_args():
     parser.add_argument("-m", "--motifs", help="input motifs filename", required=True, type=str)
     return parser.parse_args()
 args = get_args()
-print(args)
 
 
 # CLASSES -------------------------------------------------------------------------------------------
@@ -23,12 +24,56 @@ class ListofGenes:
     def set_up_gene(self, header, sequence):
         gene_obj = Gene(header, sequence)
         self.genes.append(gene_obj)
+    
+    def draw_gene_bases(self, x_margin, gene_height):
+
+        num_genes = len(self.genes)
+        longest_gene_len = max([gene.length for gene in self.genes])
+
+        surface_width = longest_gene_len + (2 * x_margin)
+        # extra space at bottom for key
+        surface_height = (num_genes + 1) * (gene_height * 3)
+
+        surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, surface_width, surface_height)
+        ctx = cairo.Context(surface)
+
+        # black
+        ctx.set_source_rgb(0, 0, 0)
+
+        # draw introns
+        for ind, gene in enumerate(self.genes):
+        
+            # set thickness of line
+            ctx.set_line_width(2)
+            # round ends of line
+            ctx.set_line_cap(cairo.LINE_CAP_ROUND)
+
+            # calculate y-location of line
+            y_loc = (ind * (gene_height * 3)) + (gene_height * (1.5))
+
+            # define location of line
+            ctx.move_to(x_margin, y_loc) # start
+            ctx.line_to(x_margin + gene.length, y_loc) # end
+
+            # draw line
+            ctx.stroke()
+
+        return surface
+
 
         
 class Gene:
     def __init__(self, header, sequence):
         self.header = header
         self.sequence = sequence
+        self.length = len(sequence)
+        self.get_exon_loc()
+
+
+    def get_exon_loc(self):
+        exon_match = re.search(r"[A-Z]+", sequence)
+        self.exon =  [exon_match.start() + 1, exon_match.end()]
+        self.exon_seq = sequence[exon_match.start(): exon_match.end()]
 
 
 
@@ -64,3 +109,10 @@ with open(args.fasta, 'r') as fasta:
             sequence += line
     # set up Gene object for last entry in FASTA file
     all_genes.set_up_gene(header, sequence)
+
+    #print([gene.exon for gene in all_genes.genes])
+    #print([gene.exon_seq for gene in all_genes.genes])
+
+
+    surface = all_genes.draw_gene_bases(20, 20)
+    surface.write_to_png("motif_mark_output.png")
